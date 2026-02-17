6adecf7e6d0d27bedea73cec1bb51de881b0987d
SimoneZoroddu <lenviur@gmail.com>
2026-02-17 13:08:01 +0100

    first commit

---
+++ main.js
@@ -0,0 +1,28 @@
+console.log('funge');
+
+const bicycles = [
+    {
+        name: 'Giacomo',
+        weight: 20
+    },
+    {
+        name: 'Giacomo',
+        weight: 18
+    },
+    {
+        name: 'Giacomo',
+        weight: 16
+    },
+    {
+        name: 'Giacomo',
+        weight: 40
+    },
+    {
+        name: 'Giacomo',
+        weight: 10
+    },
+    {
+        name: 'Giacomo',
+        weight: 8
+    }
+]
